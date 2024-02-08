ceaf487c871361b78b6539513181053d9bbf91ef
Lawrence Splaver <lsplaver263@gmail.com>
2024-02-08 10:42:53 -0500

    Added AddIngrediantForm forrn.

---
+++ WinFormsApp1/Forms/ModifyForms/ModifyIngrediants/ModifyIngrediantForm/AddIngrediantForm.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Recipes.Forms.ModifyForms.ModifyIngrediants.ModifyIngrediantForm
+{
+    partial class frmAddIngrediantForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            txtIngrediantForm = new TextBox();
+            btnAddIngrediantForm = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(115, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Ingrediant Form";
+            // 
+            // txtIngrediantForm
+            // 
+            txtIngrediantForm.Location = new Point(182, 6);
+            txtIngrediantForm.Name = "txtIngrediantForm";
+            txtIngrediantForm.Size = new Size(203, 27);
+            txtIngrediantForm.TabIndex = 1;
+            // 
+            // btnAddIngrediantForm
+            // 
+            btnAddIngrediantForm.Location = new Point(12, 46);
+            btnAddIngrediantForm.Name = "btnAddIngrediantForm";
+            btnAddIngrediantForm.Size = new Size(373, 29);
+            btnAddIngrediantForm.TabIndex = 2;
+            btnAddIngrediantForm.Text = "Add Ingrediant Form";
+            btnAddIngrediantForm.UseVisualStyleBackColor = true;
+            btnAddIngrediantForm.Click += btnAddIngrediantForm_Click;
+            // 
+            // frmAddIngrediantForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(442, 105);
+            Controls.Add(btnAddIngrediantForm);
+            Controls.Add(txtIngrediantForm);
+            Controls.Add(label1);
+            Name = "frmAddIngrediantForm";
+            Text = "Add Ingrediant Form";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox txtIngrediantForm;
+        private Button btnAddIngrediantForm;
+    }
+}
---
+++ WinFormsApp1/Forms/ModifyForms/ModifyIngrediants/ModifyIngrediantForm/AddIngrediantForm.cs
@@ -0,0 +1,50 @@
+﻿using Recipes.Models.DataLayer;
+using Recipes.Objects;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Recipes.Forms.ModifyForms.ModifyIngrediants.ModifyIngrediantForm
+{
+    public partial class frmAddIngrediantForm : Form
+    {
+        private ServerObject ServerObject { get; set; }
+        public frmAddIngrediantForm(ServerObject serverObject)
+        {
+            ServerObject = serverObject;
+            InitializeComponent();
+        }
+
+        private void btnAddIngrediantForm_Click(object sender, EventArgs e)
+        {
+            RecipesContext context = new RecipesContext(ServerObject);
+            bool isIncluded = false;
+            foreach (Ingrediantform i in context.Ingrediantforms)
+            {
+                if (i.IngrediantForm1 == txtIngrediantForm.Text)
+                {
+                    isIncluded = true;
+                    break;
+                }
+            }
+            if (!isIncluded)
+            {
+                Ingrediantform ingrediantform = new Ingrediantform();
+                ingrediantform.IngrediantForm1 = txtIngrediantForm.Text.ToLower();
+                context.Ingrediantforms.Add(ingrediantform);
+                context.SaveChanges();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show(txtIngrediantForm.Text + " is already in the database.");
+            }
+        }
+    }
+}
